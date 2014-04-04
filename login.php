edd91180e90835da60e59986c6d2b544851970bf
hikaru100ten <hikaru100ten@yahoo.co.jp>
2014-04-04 22:10:10 +0900

    first

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTDxhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Pikatter-ログイン</title>
+</head>
+<body>
+<center>
+<?php
+    print '<font color="#0F0FFF" size="7">Pikatterへようこそ</font><br />';
+    print 'TwitterのID、パスワードを入力してください';
+?>
+</center>
+</body>
+</html>
